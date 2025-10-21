4bfecbae3d5328d3ef6919e6e6b46fc090b2e1e7
Diaz <canosadiaz6@gmail.com>
2025-10-21 17:49:00 +0200

    SegundoIntento

--- src/EjerciciosBasicos3.java
+++ src/EjerciciosBasicos3.java
@@ -21,10 +21,19 @@ public class EjerciciosBasicos3 {
             System.out.println("\nNº ataque " +nAtaque);
             System.out.println("Ataca pokemon 1");
             vida1 = vida1 - danho2;
-            System.out.println("Ataca el pokemon 1 y hace este daño:" + danho1);
+            System.out.println("Ataca el pokemon 1 y hace este daño: " + danho1);
             System.out.println("Ataca pokemon 2");
             vida2 = vida2 - danho1;
-            System.out.println("Ataca el pokemon 2 y hace este daño:" + danho2);
+            System.out.println("Ataca el pokemon 2 y hace este daño: " + danho2);
+
+            if (vida1 < 0) {
+                vida1 = 0;
+
+            }
+            if  (vida2 < 0) {
+                vida2 = 0;
+            }
+
             System.out.println("Al pokemon 1 le quedan " +vida1+ " de vida y al pokemon 2 le quedan " +vida2+ " de vida.");
 
         }
